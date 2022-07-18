595aa4473900ec08746a78b017bacf5bd0b590ee
Camille Gachido <camillegachido70@gmail.com>
2022-07-18 17:55:49 -0300

    add: packages and initial configs

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Organizze - compartilhe seus estudos</title>
+        <meta
+          name="description"
+          content="Organize e compartilhe seus estudos"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
